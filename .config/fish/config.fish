2350d8dd8c37255b4abae861ba7ab107ee189a68
wylliemorth <rodrigosantosmorgado@gmail.com>
2021-11-03 22:46:38 +0000

    Added bash, zsh, fish, kitty, mpv, alacritty, starship

---
+++ .config/fish/config.fish
@@ -0,0 +1,73 @@
+set fish_greeting
+set VIRTUAL_ENV_DISABLE_PROMPT "1"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    source ("/usr/bin/starship" init fish --print-full-init | psub)
+end
+
+## FUNCTIONS
+# Fish command history
+function history
+    builtin history --show-time='%F %T '
+end
+
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+
+## ALIASES
+# Replace ls with exa
+alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
+
+# Replace cat with bat
+alias cat='bat --style header --style rules --style snip --style changes --style header'
+
+# Recent installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
+# Sort installed packages according to size in MB
+alias big="expac -H M '%m\t%n' | sort -h | nl"
+
+# Other amenities
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias rm="rm -v"
+
+alias xclip='xclip -selection c'
+alias config='/usr/bin/git --git-dir=/home/wylliem/dotfiles/ --work-tree=/home/wylliem'
--- README.md
+++
@@ -1,2 +0,0 @@
-# dotfiles
-Backup of my dotfiles
