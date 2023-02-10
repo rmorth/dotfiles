817f0a6fd106d9d454671c3498acea5c2f7636a8
Rodrigo Morgado <rodrigosantosmorgado@gmail.com>
2023-02-10 20:51:45 +0000

    bat: change theme to gruvbox

--- zsh/bat.zsh
+++ zsh/bat.zsh
@@ -1,5 +1,5 @@
 if [ "$(command -v bat)" ]; then
-	export BAT_THEME="Dracula"
+	export BAT_THEME="gruvbox-dark"
 	alias cat='bat --style header --style rule --style snip --style changes --style header'
 
 	# pretty man pages :)
