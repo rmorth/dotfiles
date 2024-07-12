45c5b7f5e79f932d68a85afcb0d044028a1e5ce2
Rodrigo Morgado <33036795+rmorth@users.noreply.github.com>
2024-07-12 21:02:25 +0100

    nvim: colorscheme fix
    Switch dark/light more easily

--- .config/nvim/lua/rmorgado/plugins/colorscheme.lua
+++ .config/nvim/lua/rmorgado/plugins/colorscheme.lua
@@ -2,8 +2,15 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000, -- make sure to load first
+    opts = {
+        flavour = "auto",
+        background = {
+            light = "latte",
+            dark = "mocha",
+        }
+    },
     config = function()
         -- load the colorscheme here
-        vim.cmd([[colorscheme catppuccin-mocha]])
+        vim.cmd([[colorscheme catppuccin]])
     end
 }
