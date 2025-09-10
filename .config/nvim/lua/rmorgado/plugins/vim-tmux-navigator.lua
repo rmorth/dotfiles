5934a4ea47f78dd5e182549118f1d3c47f2686b7
Rodrigo Morgado <33036795+rmorth@users.noreply.github.com>
2025-09-10 21:39:35 +0100

    nvim: fix tmux navigator
    This sort of fixes terminal mode but not really, it's less annoying this
    way though.

--- .config/nvim/lua/rmorgado/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/rmorgado/plugins/vim-tmux-navigator.lua
@@ -1,13 +1,26 @@
 return {
     "christoomey/vim-tmux-navigator",
-    config = function ()
-        local noremap = {
-            noremap = true
-        }
 
-        vim.api.nvim_set_keymap("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", noremap)
-        vim.api.nvim_set_keymap("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", noremap)
-        vim.api.nvim_set_keymap("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", noremap)
-        vim.api.nvim_set_keymap("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", noremap)
-    end
+    -- set before the plugin loads
+    init = function()
+        vim.g.tmux_navigator_no_mappings = 1
+    end,
+
+    config = function()
+        local map = vim.keymap.set
+        local opts = { silent = true, noremap = true }
+
+        -- normal-mode mappings
+        map('n', '<C-h>', '<Cmd> TmuxNavigateLeft<CR>',  opts)
+        map('n', '<C-j>', '<Cmd> TmuxNavigateDown<CR>',  opts)
+        map('n', '<C-k>', '<Cmd> TmuxNavigateUp<CR>',    opts)
+        map('n', '<C-l>', '<Cmd> TmuxNavigateRight<CR>', opts)
+
+        -- terminal-mode mappings: leave terminal first, then navigate (to avoid conflicts with ZSH bindings)
+        map('t', '<C-h>', [[<C-\><C-n><Cmd> TmuxNavigateLeft<CR>]],  opts)
+        map('t', '<C-j>', [[<C-\><C-n><Cmd> TmuxNavigateDown<CR>]],  opts)
+        map('t', '<C-k>', [[<C-\><C-n><Cmd> TmuxNavigateUp<CR>]],    opts)
+        map('t', '<C-l>', [[<C-\><C-n><Cmd> TmuxNavigateRight<CR>]], opts)
+    end,
 }
+
